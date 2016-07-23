a9a7430f42ae65c3424fab234bd2f4a98eca4269
Fabio VM <mpc.fabio@gmail.com>
2016-07-23 18:40:38 -0300

    Commit Inicial do projeto padrão ASP.Net MVC Visual Studio 2015

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoletoMVC.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoletoMVC.Startup))]
+namespace BoletoMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
